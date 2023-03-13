689f6f07d3a652bd0842821145a8ee11b389d461
daya8789 <114335626+daya8789@users.noreply.github.com>
2023-03-13 20:01:50 +0530

    Add files via upload

---
+++ FirstClass.java
@@ -0,0 +1,14 @@
+
+public class FirstClass{
+
+    public static void main(String[] args){
+
+        System.out.println("hello world !");
+        for(int i=0; i<10; i++){
+            System.out.println("*");
+        }
+        for(int i=0; i<10; i++){
+            System.out.print("* ");
+        }
+    }
+}
